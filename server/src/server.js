00ff5b2274278b988f634e6f7ef427df1888c726
Tanish Tyagi <tanishtyagi887@gmail.com.com>
2026-06-04 11:12:02 +0530

    setup express and socket.io server

---
+++ server/src/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User Connected:", socket.id);
+});
+
+server.listen(5000, () => {
+  console.log("Server Running On Port 5000");
+});
