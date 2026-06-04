f2f40f9100ccaef89c5d6247d7703ffeac9f6ac6
Tanish Tyagi <tanishtyagi887@gmail.com.com>
2026-06-04 14:24:37 +0530

    Connect frontend with Socket.IO

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,44 @@
+import { useState, useEffect } from "react";
+import { socket } from "../socket/socket";
+
 function Home() {
+  const [name, setName] = useState("");
+  const [roomId, setRoomId] = useState("");
+
+  const createRoom = () => {
+    socket.emit("create_room", {
+      name,
+    });
+  };
+
+  useEffect(() => {
+    socket.on("room_created", (data) => {
+      setRoomId(data.roomId);
+    });
+
+    return () => {
+      socket.off("room_created");
+    };
+  }, []);
+
   return (
     <div>
       <h1>SketchSync</h1>
 
       <input
+        type="text"
         placeholder="Enter Name"
+        value={name}
+        onChange={(e) =>
+          setName(e.target.value)
+        }
       />
 
-      <button>
+      <button onClick={createRoom}>
         Create Room
       </button>
 
-      <button>
-        Join Room
-      </button>
+      <h2>{roomId}</h2>
     </div>
   );
 }
