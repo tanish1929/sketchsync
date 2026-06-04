7a86ef0130e92119717efe1a9a76a43ddfbf196a
Tanish Tyagi <tanishtyagi887@gmail.com.com>
2026-06-04 13:22:39 +0530

    Setup React frontend

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+function Home() {
+  return (
+    <div>
+      <h1>SketchSync</h1>
+
+      <input
+        placeholder="Enter Name"
+      />
+
+      <button>
+        Create Room
+      </button>
+
+      <button>
+        Join Room
+      </button>
+    </div>
+  );
+}
+
+export default Home;
