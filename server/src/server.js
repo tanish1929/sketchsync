423f6dbd1cb3b0cc92640d4a605dede401344f13
Tanish Tyagi <tanishtyagi887@gmail.com.com>
2026-06-05 17:57:32 +0530

    Add health check route

--- server/src/server.js
+++ server/src/server.js
@@ -1,29 +1,22 @@
-require("dotenv").config();
-
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
 
-const server =
-  http.createServer(app);
+app.get("/", (req, res) => {
+  res.send("SketchSync Backend Running 🚀");
+});
 
 const io = new Server(server, {
   cors: {
-    origin:
-      process.env.CLIENT_URL,
-    credentials: true,
+    origin: "*",
   },
 });
 
-require("./socket/socketHandler")(io);
+const PORT = process.env.PORT || 5000;
 
-server.listen(
-  process.env.PORT || 5000,
-  () => {
-    console.log(
-      "Server running on port 5000"
-    );
-  }
-);
+server.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
